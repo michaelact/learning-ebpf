08432c95d607441308afe9e501b13366ecb3859c
michaelact <86778470+michaelact@users.noreply.github.com>
2023-12-12 17:29:17 +0700

    feat: gets triggered by more than one syscall

--- chapter2/hello-map.py
+++ chapter2/hello-map.py
@@ -22,8 +22,10 @@ int hello(void *ctx) {
 """
 
 b = BPF(text=program)
-syscall = b.get_syscall_fnname("execve")
-b.attach_kprobe(event=syscall, fn_name="hello")
+
+for syscall_fnname in ["execve", "openat", "write"]:
+   syscall = b.get_syscall_fnname(syscall_fnname)
+   b.attach_kprobe(event=syscall, fn_name="hello")
 
 # Attach to a tracepoint that gets hit for all syscalls 
 # b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
